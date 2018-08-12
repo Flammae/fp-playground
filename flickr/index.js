b44d229eefc4ba080760f5b732fefb5fbdd2eb54
beqa <b.meqvabishvili159@gmail.com>
2018-08-12 05:38:44 -0800

    flickr api project

---
+++ flickr/index.js
@@ -0,0 +1,40 @@
+const impure = {
+  getJson: R.curry((cb, url) => $.getJSON(url, cb)),
+  setHtml: R.curry((sel, html) => $(sel).html(html)),
+  trace: R.curry((tag, x) => {
+    console.log(tag, x);
+    return x;
+  })
+};
+
+// pure
+const host = "api.flickr.com";
+const path = "/services/feeds/photos_public.gne";
+const query = t => `?tags=${t}&format=json&jsoncallback=?`;
+const url = t => `https://${host}${path}${query(t)}`;
+
+const image = src => $("<img />", { src });
+const mediaUrl = R.compose(
+  R.prop("m"),
+  R.prop("media")
+);
+const mediaUrls = R.compose(
+  R.map(mediaUrl),
+  R.prop("items")
+);
+const images = R.compose(
+  R.map(image),
+  mediaUrls
+  // impure.trace
+);
+
+// impure
+const render = R.compose(
+  impure.setHtml("#main"),
+  images
+);
+const app = R.compose(
+  impure.getJson(render),
+  url
+);
+app("nicolas cage");
